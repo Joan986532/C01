c3da80d60bcd36be52cad83c88310b4c1539f6f5
Joannn <joannn@vbox.myguest.virtualbox.org>
2025-07-14 13:33:04 +0200

    v1 ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <fstream>
 
 int	main(int argc, char **argv)
 {
+	std::string	str;
+	std::string	newFile;
+	std::string	s1;
+	std::string	s2;
+	size_t		pos;
+	size_t		size;
+
 	if (argc != 4)
 		return (1);
+	s1 = argv[2];
+	s2 = argv[3];
+	size = s1.length();
+	std::ifstream myFile(argv[1]);
+	if (myFile.is_open())
+	{
+		newFile = argv[1];
+		newFile += "_replace";
+		std::ofstream fileReplace(newFile/*, std::fstream::app*/);
+		while (getline(myFile, str))
+		{
+			//std::cout << str << std::endl;
+			 while (1)
+			{
+				pos = str.find(s1);
+				//std::cout << "pos: " << pos << std::endl;
+				if (pos == std::string::npos)
+					break ;
+				str.erase(pos, size);
+				//std::cout << str << std::endl;
+				str.insert(pos, s2);
+				//std::cout << str << std::endl;
+			}
+			fileReplace << str << std::endl;
+		}
+		fileReplace.close();
+		myFile.close();
+	}
+	return (0);
 }
